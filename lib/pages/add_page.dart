4e62705ab2753a3880311e5ca22401474b9e7438
Aditya Rohman <aditya@Adityas-MacBook-Pro.local>
2020-02-20 18:04:00 +0700

    second commit

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -24,8 +24,90 @@ class _AddPageState extends State<AddPage> {
           ),
         ),
         body: Center(
-          child: Text('Halaman Kosong'),
+          child: _formAddFoodStore(),
         ),
+      ),
+    );
+  }
+
+  Widget _formAddFoodStore() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'images/add_background.png',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 60.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              hintText: 'Nama',
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              hintText: 'Alamat',
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              hintText: 'Deskripsi',
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              hintText: 'Nomor Handphone',
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              hintText: 'Link Foto',
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          RaisedButton(
+            padding: EdgeInsets.symmetric(vertical: 14.0),
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(
+              'Daftar',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            color: Colors.red[400],
+            onPressed: () {
+              // NOT IMPLEMENT YET!
+            },
+          ),
+        ],
       ),
     );
   }
