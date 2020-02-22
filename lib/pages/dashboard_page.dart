d7f13edcc327a1aece3913623cca6aeab0f22f27
Aditya Rohman <aditya@Adityas-MacBook-Pro.local>
2020-02-22 21:07:35 +0700

    v1.5 alpha test commit

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,4 @@
 /* IMPORT PACKAGE */
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:waroeng_app/services/fire_auth_service.dart';
